cfc61e66b86062010dccccd020b175bdf5d3e842
50Sm <aakishev97@gmail.com>
2023-08-18 18:15:13 +0600

    Добавил еще один HTTP метод

--- main.go
+++ main.go
@@ -42,10 +42,34 @@ func createHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
+func updateHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPut {
+		http.Error(w, "Ожидается PUT-запрос", http.StatusMethodNotAllowed)
+		return
+	}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Ошибка чтения тела запроса", http.StatusInternalServerError)
+		return
+	}
+	defer r.Body.Close()
+
+	savedData = string(body) // Сохраняем данные в глобальной переменной
+	fmt.Printf("Получено PUT-тело: %s\n", savedData)
+
+	response := ResponseData{
+		Message: "Запись перезаписана",
+		Data:    savedData,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
 func main() {
 	http.HandleFunc("/hello", helloHandler)   // обработчик пути
 	http.HandleFunc("/create", createHandler) // второй эндпоинт
-
+	http.HandleFunc("/update", updateHandler) // третий эндпоинт
 	fmt.Println("Сервер запущен на порту 8080")
 	http.ListenAndServe(":8080", nil)
 }
